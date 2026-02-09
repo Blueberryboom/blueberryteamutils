34d212324282e21ecd64358e4cdfbde0b30514af
Blueberryboom <152311332+Blueberryboom@users.noreply.github.com>
2026-02-09 21:27:04 +0000

    Create schema.sql

---
+++ database/schema.sql
@@ -0,0 +1,33 @@
+CREATE TABLE IF NOT EXISTS tickets (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  ticket_id VARCHAR(10),
+  user_id VARCHAR(32),
+  username VARCHAR(64),
+
+  type ENUM('partner','creator','mod','other'),
+
+  channel_id VARCHAR(32),
+
+  status ENUM('open','claimed','closed') DEFAULT 'open',
+
+  claimed_by VARCHAR(32) NULL,
+
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  closed_at TIMESTAMP NULL,
+
+  close_reason TEXT NULL
+);
+
+CREATE TABLE IF NOT EXISTS ticket_logs (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  ticket_id VARCHAR(10),
+
+  action ENUM('claim','close','user_add'),
+
+  staff_id VARCHAR(32),
+  staff_name VARCHAR(64),
+
+  extra TEXT NULL,
+
+  time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
